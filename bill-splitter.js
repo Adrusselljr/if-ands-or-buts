6a5b0c956a19116fb50466fed592b17c200140c1
Aaron Russell Jr <adrusselljr@Aarons-MBP.lan>
2021-09-22 15:51:34 -0400

    edit js

--- bill-splitter.js
+++ bill-splitter.js
@@ -3,14 +3,20 @@ const prompt = require("prompt-sync")()
 const person1Paid = Number(prompt("Please enter the amount Person 1 paid: "))
 const person2Paid = Number(prompt("Please enter the amount Person 2 paid: "))
 
+let total = 0
 let owed = 0
+let each = 0
 
 if(person1Paid > person2Paid) {
-    owed = person1Paid - person2Paid
+    total = person1Paid + person2Paid
+    each = total / 2
+    owed = each - person2Paid
     console.log(`Person 2 owes Person 1 $${owed}`)
 }
 else if(person2Paid > person1Paid) {
-    owed = person2Paid - person1Paid
+    total = person2Paid + person1Paid
+    each = total / 2
+    owed = each - person1Paid
     console.log(`Person 1 owes Person 2 $${owed}`)
 }
 else {
